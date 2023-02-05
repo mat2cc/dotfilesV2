534f39579ff9ebde034cb95b8c311416df3c9e72
Matt Christofides <matt.christofides@routethis.com>
2023-02-05 12:48:57 -0500

    feat: added shortcut keys for harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -13,4 +13,8 @@ local ui = require("harpoon.ui")
 
 nnoremap("<leader>a", function() mark.add_file() end)
 nnoremap("<C-e>", function() ui.toggle_quick_menu() end)
-
+nnoremap("<leader>ha", function() ui.nav_file(1) end)
+nnoremap("<leader>ho", function() ui.nav_file(2) end)
+nnoremap("<leader>he", function() ui.nav_file(3) end)
+nnoremap("<leader>hu", function() ui.nav_file(4) end)
+nnoremap("<leader>hi", function() ui.nav_file(5) end)
